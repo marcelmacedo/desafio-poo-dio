e3b44456835d03fbaa4c32bfbfe38b8723422ced
marcelmacedo <marcel.handock@gmail.com>
2022-11-26 17:58:55 -0400

    Abstração e encapsilamento

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.lang.reflect.Member;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao do curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descricao do curso");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descricao mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        System.out.println(mentoria1);
+
+    }
+}
